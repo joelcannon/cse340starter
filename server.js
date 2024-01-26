aa05b7b169ec41c3311d07d48dd63d2b6e3966ee
Joel Cannon <joelcannon@mac.com>
2024-01-25 18:01:02 -0600

    robust error handling

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.set("layout", "./layouts/layout"); // not at views root
 app.use(static);
 
 // Index Route
-app.get("/", baseController.buildHome);
+app.get("/", utilities.handleErrors(baseController.buildHome));
 
 // Inventory routes
 app.use("/inv", inventoryRoute);
@@ -38,15 +38,21 @@ app.use(async (req, res, next) => {
 });
 
 /* ***********************
- * Express Error Handler
- * Place after all other middleware
- *************************/
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav();
   console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if (err.status == 404) {
+    message = err.message;
+  } else {
+    message = "Oh no! There was a crash. Maybe try a different route?";
+  }
   res.render("errors/error", {
     title: err.status || "Server Error",
-    message: err.message,
+    message,
     nav,
   });
 });
