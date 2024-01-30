4a18cc6aa01b5da39b0cc9c2fdc70c6c40067894
Joel Cannon <joelcannon@mac.com>
2024-01-29 20:54:52 -0600

    w4.2 login view

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
 const inventoryRoute = require("./routes/inventoryRoute");
+const accountRoute = require("./routes/accountRoute");
 const baseController = require("./controllers/baseController");
 const utilities = require("./utilities");
 const session = require("express-session");
@@ -56,6 +57,9 @@ app.get("/", utilities.handleErrors(baseController.buildHome));
 
 // Inventory routes
 app.use("/inv", inventoryRoute);
+
+// Account routes
+app.use("/account", accountRoute);
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
