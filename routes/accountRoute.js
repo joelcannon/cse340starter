8ebfcbe6f95cd64bbab1de55b82ee500631e9e54
Joel Cannon <joelcannon@mac.com>
2024-02-01 19:50:25 -0600

    add email check and stickyness

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities");
 const accountController = require("../controllers/accountController");
+const regValidate = require("../utilities/account-validation");
 
 // Route to build login page
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -16,7 +17,14 @@ router.get(
 // Route to process register page
 router.post(
   "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
   utilities.handleErrors(accountController.registerAccount)
 );
+
+// Process the login attempt
+router.post("/login", (req, res) => {
+  res.status(200).send("login process");
+});
 
 module.exports = router;
