07803ef14eacb8a6be5bc8189a7754d467a47147
Joel Cannon <joelcannon@mac.com>
2024-02-07 18:42:58 -0600

    add checkJWTToken to all paths and simplify classification select.

--- server.js
+++ server.js
@@ -42,10 +42,10 @@ app.use(function (req, res, next) {
   res.locals.messages = require("express-messages")(req, res);
   next();
 });
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cookieParser());
+app.use(utilities.checkJWTToken);
 
 /* ***********************
  * View Engine and Templates
--- utilities/index.js
+++ utilities/index.js
@@ -1,5 +1,7 @@
 const invModel = require("../models/inventory-model");
 const Util = {};
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 /* ************************
  * Constructs the nav HTML unordered list
@@ -21,11 +23,9 @@ Util.getClassificationOptions = async function (selectedId) {
   let data = await invModel.getClassifications();
   let options = "";
   data.rows.forEach((row) => {
-    if (Number(row.classification_id) === Number(selectedId)) {
-      options += `<option value="${row.classification_id}" selected>${row.classification_name}</option>`;
-    } else {
-      options += `<option value="${row.classification_id}">${row.classification_name}</option>`;
-    }
+    const isSelected =
+      Number(row.classification_id) === Number(selectedId) ? "selected" : "";
+    options += `<option value="${row.classification_id}" ${isSelected}>${row.classification_name}</option>`;
   });
   return options;
 };
@@ -98,5 +98,29 @@ Util.buildCarDetail = async function (data) {
  **************************************** */
 Util.handleErrors = (fn) => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next);
+
+/* ****************************************
+ * Middleware to check token validity
+ **************************************** */
+Util.checkJWTToken = (req, res, next) => {
+  if (req.cookies.jwt) {
+    jwt.verify(
+      req.cookies.jwt,
+      process.env.ACCESS_TOKEN_SECRET,
+      function (err, accountData) {
+        if (err) {
+          req.flash("Please log in");
+          res.clearCookie("jwt");
+          return res.redirect("/account/login");
+        }
+        res.locals.accountData = accountData;
+        res.locals.loggedin = 1;
+        next();
+      }
+    );
+  } else {
+    next();
+  }
+};
 
 module.exports = Util;
